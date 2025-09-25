d1386385239dc048c1c37ba7aaa4f3c35a167494
Smith Bentley <825507@stma.k12.mn.us>
2025-09-25 13:15:41 -0500

    message

--- Assingments/TopFiveMovies.py
+++ Assingments/TopFiveMovies.py
@@ -1,2 +1,9 @@
 #assignment
+print("Top Five Movies")
+print("napoleon dynamite")
+print("captain america")
+print("the incredables")
+print("mega mind")
+print("superman")
+
 
---
+++ Practice/2.3-practice.py
@@ -0,0 +1,30 @@
+# String - "in quotations"
+# Integer - a whole number
+# Float - a decimal number ex 3.14159265358979323846264
+# Char - a single glyph ex. 'c'
+#Bool - true or false
+
+# How tot define a variable!!
+# <name> = <value>
+
+lowercase = False
+UPPERCASE = False
+camelcase = False # All lowercase, no spaces, capital for new words.
+UpperCamelCase = False
+lowerCamelCase = False
+SNAKE_CASE = True
+snake_case = True
+SCREAMING_SNAKE_CASE = True
+
+# Other general rules to naming things
+# 1.Concise
+#   -
+
+the_font_size_for_paragraphs = 10
+font_size_para = 10
+para_size = 10
+para = 10
+
+
+print(para)
+input()
