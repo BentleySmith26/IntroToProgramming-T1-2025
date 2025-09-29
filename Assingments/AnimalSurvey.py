6210fe822630db28c031d02e5cf8c9b2637775a9
Smith Bentley <825507@stma.k12.mn.us>
2025-09-29 13:20:12 -0500

    update to animal survey to include notes

---
+++ Assingments/AnimalSurvey.py
@@ -0,0 +1,12 @@
+fav_animal = input("What is your favorite animal?\n> ")
+fav_reason = input("Why is this animal your favorite?\n> ")
+fun_fact = input("What is a cool fact about your chosen animal?\n> ")
+own = input("Do you own this animal?\n> ")
+found = input("Where is your animal typically found?\n> ")
+diet = input("Is you animal a Carnivore, Herbivore, or a Omnivore?\n> ")
+
+#the lines above help display the final data in the summary at the end
+
+print( f"Your favorite animal is the {fav_animal} Because {fav_reason}. One fun fact is that {fun_fact}. Your ownership status is {own}. This animal is typically found {found} Finally this animal is a {diet}.")
+
+#this line displays the final summary
---
+++ Practice/2.4-practice.py
@@ -0,0 +1,11 @@
+first_name = "Bentley"
+
+last_name = "Smith"
+
+print("Hello, " + first_name + " " + last_name + "!")
+
+name = "Bentley"
+age = 17
+
+print("Name: " + name + ", Age: " + str(age))
+
