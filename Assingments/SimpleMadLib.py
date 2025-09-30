4884bdfe5036b3585d9f28c0a6836a1a3e436c29
Smith Bentley <825507@stma.k12.mn.us>
2025-09-30 13:47:47 -0500

    updating mad lib assignment

---
+++ Assingments/SimpleMadLib.py
@@ -0,0 +1,27 @@
+
+#Base madlib
+#One time in {Noun_Place} There was a person named {Name}.
+#and at around {Time}, there was a wild {Noun_thing} raging through {First - Noun_Place}.
+#Thankfully a {Adjective} {Noun_Animal} came to Save the day!
+#as the {Adjective} Battle came to the end, a plot twist arose and the real winner was a {Adjective} {Noun_Animal}
+#Everybody Cheered as the {second - Noun_Animal} came back from {Noun_place}
+#as the {adjective} day finished everybody sat back and enjoyed a nice glass of {noun_thing}
+
+noun_p1 = input("Please give a noun that is a place\n> ")
+name = input("Please give a name\n> ")
+time = input("Please give a time\n> ")
+noun_t1 = input("Please give a noun that is a thing\n> ")
+adjective1 = input("Please give a adjective\n> ")
+noun_a1 = input("please give a noun that is an animal\n> ")
+adjective2 = input("Please give another adjective\n> ")
+adjective3 = input("Please give another adjective\n> ")
+noun_a2 = input("Please give another animal\n> ")
+noun_p2 = input("Please give another noun that is a place\n> ")
+adjective4 = input("Please give another adjective\n> ")
+noun_t2 = input("Please give another noun which is a thing\n> ")
+
+
+print(f"One time in {noun_p1} There was a person named {name}. And at around {time}, there was a wild {noun_t1} raging through {noun_p1}. Thankfully a {adjective1} {noun_a1} came to save the day! as the {adjective2} Battle came to the end, a plot twist arose and the real winner was a {adjective3} {noun_a2}. Everybody Cheered as the {noun_a2} came back from {noun_p2}. as the {adjective4} day finished everybody sat back and enjoyed a nice glass of {noun_t2}.")
+
+
+
